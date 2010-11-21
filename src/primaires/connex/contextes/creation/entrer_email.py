d66d4dad54d5c195775b88ae5d47131cc62cd4fc
kredh <kredh@free.fr>
2010-11-21 20:36:23 +0100

    Correction d'une erreur dans la regex de l'e-mail valide     Aucun délimiteur ^ ou $ n'était spécifié. A la place se trouvaient des     signes "\b". On pouvait rentrer une adresse "kredh@free.fr$"     (rendu compte par erreur). Les signes "\b" ont été supprimés et remplacer     par un signe ^ au début de la chaîne et un signe $ à la fin.

--- src/primaires/connex/contextes/creation/entrer_email.py
+++ src/primaires/connex/contextes/creation/entrer_email.py
@@ -34,7 +34,7 @@ import re
 
 ## Constantes
 # Regex
-RE_MAIL_VALIDE = re.compile(r"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b", re.I)
+RE_MAIL_VALIDE = re.compile(r"^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$", re.I)
 
 class EntrerEmail(Contexte):
     """Contexte du choix de l'adresse mail.
