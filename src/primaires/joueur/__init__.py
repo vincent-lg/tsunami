c8e19aa6988d101a5ca7fe5b82b70a3fd1534899
DAVY Guillaume <davyg2@gmail.com>
2011-04-06 17:43:18 +0200

    Oublie de parenthèse mis en avant par le dernier commit

--- src/primaires/joueur/__init__.py
+++ src/primaires/joueur/__init__.py
@@ -103,5 +103,5 @@ class Module(BaseModule):
         """
         for joueur in self.importeur.connex.joueurs:
             i_c = joueur.instance_connexion
-            if joueur.est_connecte() and i_c is None or not i_c.est_connecte():
+            if joueur.est_connecte() and (i_c is None or not i_c.est_connecte()):
                 joueur.pre_deconnecter()
