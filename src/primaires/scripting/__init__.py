f8eca81a2d27eca733b457607b34e01d15abfe6a
kredh <kredh@free.fr>
2011-07-05 08:18:43 +0200

    Ajout du module scripting (en prototype)

---
+++ src/primaires/scripting/__init__.py
@@ -0,0 +1,105 @@
+# -*-coding:Utf-8 -*
+
+# Copyright (c) 2010 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Fichier contenant le module primaire scripting."""
+
+import re
+
+from abstraits.module import *
+from .config import cfg_scripting
+
+class Module(BaseModule):
+    
+    """Cette classe contient les informations du module primaire scripting.
+    Ce module gère le langage de script utilisé pour écrire des quêtes et
+    personnaliser certains objets de l'univers. Il gère également les
+    éditeurs et les objets gérant les quêtes.
+    
+    """
+    
+    def __init__(self, importeur):
+        """Constructeur du module"""
+        BaseModule.__init__(self, importeur, "scriptçing", "primaire")
+        self.cfg = None
+    
+    def config(self):
+        """Méthode de configuration du module"""
+        self.cfg = type(self.importeur).anaconf.get_config("scripting",
+            "scripting/syntaxe.cfg", "config scripting", cfg_scripting)
+        
+        BaseModule.config(self)
+    
+    def init(self):
+        """Initialisation"""
+        self.test_instruction("test(-1.4, \"chiwa,fok \")")
+    
+    def test_instruction(self, chaine):
+        """Test d'instruction.
+        Méthode de debug.
+        
+        """
+        cfg = self.cfg
+        identifiant = cfg.identifiant
+        type_de_donnee = "(" + ")|(".join([cfg.chaine, cfg.nombre, cfg.identifiant]) + ")"
+        affectation = cfg.affectation.format(identifiant=identifiant,
+                type_de_donnee=type_de_donnee)
+        sep = cfg.sep
+        fonction = cfg.fonction.format(nom_fonction=cfg.nom_fonction,
+                sep=sep, type_de_donnee=type_de_donnee)
+        
+        # Regex
+        print("fonction", fonction)
+        c=input()
+        reg = re.compile("^" + fonction + "$", re.DEBUG)
+        res = reg.search(chaine)
+        if res is None:
+            print("Non !")
+            return
+        
+        nom_fonction = res.groups()[0]
+        arg = res.groups()[1]
+        arg = arg or ""
+        args = []
+        if arg:
+            args.append(arg)
+        
+        chaine = chaine.strip(")")
+        chaine = chaine[len(nom_fonction) + 1 + len(arg):]
+        ARGUMENT = r"({sep}({a}))({sep}({a}))*".format(a=type_de_donnee, sep=sep)
+        RE_ARGUMENT = re.compile("^" + ARGUMENT + "$")
+        while chaine:
+            res = RE_ARGUMENT.search(chaine)
+            groupes = res.groups()
+            arg_c = groupes[0]
+            arg_n = groupes[1]
+            chaine = chaine[len(arg_c):]
+            args.append(arg_n)
+        
+        print(nom_fonction, args)
---
+++ src/primaires/scripting/config.py
@@ -0,0 +1,82 @@
+﻿# -*-coding:Utf-8 -*
+
+# Copyright (c) 2010 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Ce fichier contient la configuration par défaut du module 'scripting'."""
+
+cfg_scripting = r"""
+# Ce fichier contient la syntaxe du scripting.
+# Vous pouvez configurer ici tous les éléments de la syntaxe et ainsi,
+# configurer votre propre langage de script en fonction de vos préférences.
+# NOTE IMPORTANTE: les informations de cette configuration sont données
+# sous la forme d'expressions régulières. Si vous ne connaissez pas ce
+# langage, évitez de le modifier.
+
+## Identifiant
+# Un identifiant est une variable contenant une certaine valeur.
+# Cette variable peut être prédéfinie par l'évènement (l'objet qu'on ramasse,
+# le PNJ auquel on parle...) ou bien définit au cours du script
+# par affectation.
+identifiant = r"{A-Za-z}{A-Za-z0-9_}*"
+
+## Types de données
+# Les différents types admis
+# Chaîne de caractères :
+chaine = r"\".+\""
+
+# Nombre :
+nombre = r"-?[0-9]+\.[0-9]+"
+
+## Affectation
+# L'affectation est l'instruction qui permet d'affecter une valeur à un
+# identifiant. Exemple :
+# variable = 5 (on affecte la valeur 5 à la variable 'variable')
+# Vous pouvez utiliser :
+# {identifiant} : un identifiant
+# {type de donnée} : un type de donnée
+#                    (entier, flottant, chaîne ou un autre identifiant)
+affectation = r"{identifiant} ?= ?{type_de_donnee}"
+
+## Fonctions
+# Une fonction permet d'exécuter une opération précise (donner à un joueur
+# un objet, dire un message, attaquer, lancer un sort...)
+# Une fonction a généralement un nom et une liste d'arguments qui peut être
+# vide.
+
+# Arguments
+# Séparateur entre les arguments :
+sep = r", ?"
+
+# Nom d'une fonction
+nom_fonction = r"[a-z]{3,}"
+
+# Ligne d'une fonction
+fonction = r"({nom_fonction})\({type_de_donnee}?({sep}{type_de_donnee})*\)"
+
+"""
