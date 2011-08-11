7bab2340017ef6006d1d7ffec4216f7c2e405acf
kredh <kredh@free.fr>
2011-08-11 21:05:38 +0200

    Commit par stupidité

--- src/primaires/perso/__init__.py
+++ src/primaires/perso/__init__.py
@@ -159,7 +159,7 @@ class Module(BaseModule):
         
         """
         a_tester = list(self.importeur.connex.joueurs)
-        a_tester += list(self.importeur.pnj.prototypes.keys())
+        a_tester += list(self.importeur.pnj.prototypes.values())
         for test in a_tester:
             if test.race is race:
                 return True
