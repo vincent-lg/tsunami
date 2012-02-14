8fb167a3c2acd2dcd7841fdf9b085826d1c6c9b3
kredh <kredh@free.fr>
2012-02-14 11:27:21 +0100

    Correction du bug empêchant les éditeurs Presentation de s'afficher

--- src/bases/collections/dictionnaire.py
+++ src/bases/collections/dictionnaire.py
@@ -30,6 +30,8 @@
 
 """Fichier contenant la classe Dictionnaire détaillée plus bas."""
 
+from collections import OrderedDict
+
 class Dictionnaire:
 
     """Classe représentant un dictionnaire, très semblable aux builtins.
@@ -89,6 +91,77 @@ class Dictionnaire:
     def nettoyer(self):
         """Nettoie le dictionnaire."""
         dictionnaire = {}
+        for cle, valeur in self.dictionnaire.items():
+            # Traitement de la clé
+            if hasattr(cle, "e_existe") and not cle.e_existe:
+                continue
+            # Traitement de la valeur
+            if hasattr(valeur, "e_existe") and not valeur.e_existe:
+                continue
+            dictionnaire[cle] = valeur
+        
+        self.dictionnaire.clear()
+        self.dictionnaire.update(dictionnaire)
+
+class DictionnaireOrdonne:
+
+    """Classe représentant un dictionnaire ordonné.
+
+    La grande différence est que ce dictionnaire est susceptible de contenir,
+    en clé ou valeur, des objets prévus pour être enregistrés.
+    Ces objets contiennent simplement un attribut e_existe qui doit
+    être à True. Si il est à False, l'objet est ignoré (c'est-à-dire
+    supprimé du dictionnaire).
+
+    A chaque action sur le dictionnaire, on le nettoie.
+
+    """
+
+    def __init__(self, *args, **kw_args):
+        """Constructeur du dictionnaire."""
+        self.dictionnaire = OrderedDict(*args, **kw_args)
+        self.e_existe = True
+
+    def __getattr__(self, attr):
+        """L'attribut en question est probablement une méthode.
+
+        Dans tous les cas, on nettoie la dictionnaire avant de retourner
+        l'objet concerné que l'on cherche dans dictionnaire.
+
+        """
+        if object.__getattribute__(self, "dictionnaire"):
+            self.nettoyer()
+        return getattr(self.dictionnaire, attr)
+
+    def __getitem__(self, item):
+        self.nettoyer()
+        return self.dictionnaire[item]
+
+    def __setitem__(self, item, valeur):
+        self.dictionnaire[item] = valeur
+
+    def __delitem__(self, item):
+        del self.dictionnaire[item]
+
+    def __repr__(self):
+        self.nettoyer()
+        return repr(self.dictionnaire)
+
+    def __str__(self):
+        self.nettoyer()
+        return str(self.dictionnaire)
+
+    def __iter__(self):
+        self.nettoyer()
+        return iter(self.dictionnaire)
+
+    def __len__(self):
+        self.nettoyer()
+        return len(self.dictionnaire)
+
+    def nettoyer(self):
+        """Nettoie le dictionnaire."""
+        dictionnaire = OrderedDict()
         for cle, valeur in self.dictionnaire.items():
             # Traitement de la clé
             if hasattr(cle, "e_existe") and not cle.e_existe:
