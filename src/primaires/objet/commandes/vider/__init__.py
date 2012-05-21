8e74e52c264ea86d143c77f1e833d9810aa268ef
kredh <kredh@free.fr>
2012-05-21 14:18:05 +0200

    Correction(?) de bugs dans les commandes remplir / vider

--- src/primaires/objet/commandes/vider/__init__.py
+++ src/primaires/objet/commandes/vider/__init__.py
@@ -57,7 +57,8 @@ class CmdVider(Commande):
         plat = self.noeud.get_masque("plat")
         plat.prioritaire = True
         plat.proprietes["conteneurs"] = \
-                "(personnage.equipement.tenus, personnage.salle.objets_sol)"
+                "(personnage.equipement.inventaire, " \
+                "personnage.salle.objets_sol)"
         plat.proprietes["types"] = "('conteneur de nourriture', )"
     
     def interpreter(self, personnage, dic_masques):
