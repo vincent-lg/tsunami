a2caf79d75d1e8cb4c3f72667e71addedb64c4b1
Kredh <kredh@free.fr>
2012-11-07 17:23:43 +0100

    Ajout du squelette du module primaire 'affection'

---
+++ src/primaires/affection/__init__.py
@@ -0,0 +1,58 @@
+# -*-coding:Utf-8 -*
+
+# Copyright (c) 2012 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Ce fichier contient le module primaire affection."""
+
+from abstraits.module import *
+
+class Module(BaseModule):
+    
+    """Classe représentant le module 'affection'.
+    
+    Ce module gère les affections, c'est-à-dire ce qui affecte
+    temporairement une salle, un personnage ou un objet. Différentes
+    classes sont créées pour ces différents cas.
+    
+    Les affections peuvent à la fois être créées par le système ou
+    par les bâtisseurs. Les affections que créent le système ne peuvent
+    pas être retirées et sont souvent appelées lors d'évènement contenus
+    dans le code-même. Les affections créées par les bâtisseurs
+    doivent être appelées au cas par cas (grâce au scripting).
+    
+    Voici le plan des principales classes du module :
+        Aucune pour l'heure
+    
+    """
+    
+    def __init__(self, importeur):
+        """Constructeur du module"""
+        BaseModule.__init__(self, importeur, "affection", "primaire")
+        self.logger = type(self.importeur).man_logs.creer_logger("affection", \
+                "affections")
