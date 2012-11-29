d6f5359eece18042f186d2ea7801e238affd6057
Kredh <kredh@free.fr>
2012-11-29 15:26:21 +0100

    Ajout d'une syntaxe d'heure par quart d'heure

---
+++ src/primaires/temps/constantes.py
@@ -0,0 +1,47 @@
+﻿# -*-coding:Utf-8 -*
+
+# Copyright (c) 2010 LE GOFF Vincent
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Ce fichier contient les constantes du module temps."""
+
+NOMS_HEURES = {
+    0: "minuit {minutes}",
+    1: "une heure {minutes} {moment}",
+    2: "deux heures {minutes} {moment}",
+    3: "trois heures {minutes} {moment}",
+    4: "quatre heures {minutes} {moment}",
+    5: "cinq heures {minutes} {moment}",
+    6: "six heures {minutes} {moment}",
+    7: "sept heures {minutes} {moment}",
+    8: "huit heures {minutes} {moment}",
+    9: "neuf heures {minutes} {moment}",
+    10: "dix heures {minutes} {moment}",
+    11: "onze heures {minutes} {moment}",
+    12: "midi {masc_minutes}",
+}
