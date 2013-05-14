e5d202471215ad0b32896f51624a7c0ac17a8998
kredh <kredh@free.fr>
2013-05-14 14:58:34 +0200

    Ajout d'une fonction scripting pour calculer le facteur de poids du personnage

---
+++ src/primaires/scripting/fonctions/facteur_poids.py
@@ -0,0 +1,59 @@
+﻿# -*-coding:Utf-8 -*
+
+# Copyright (c) 2013 LE GOFF Vincent
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Fichier contenant la fonction facteur_poids."""
+
+from fractions import Fraction
+
+from primaires.scripting.fonction import Fonction
+
+class ClasseFonction(Fonction):
+
+    """Renvoie le facteur poids du personnage (entre 0 et 1)."""
+
+    @classmethod
+    def init_types(cls):
+        cls.ajouter_types(cls.facteur_poids, "Personnage")
+
+    @staticmethod
+    def facteur_poids(personnage):
+        """Retourne le facteur de poids du personnage passé en argument.
+
+        Paramètres à préciser :
+
+          * personnage : le personnage à tester (joueur ou PNJ)
+
+        Le facteur retourné est entre 0 et 1. Il correspond directement
+        au poids porté par le personnage : si il a 100% d'encombrement,
+        retourne 1. Si il a 0%, retourne 0. Si il a 50%,
+        retourne 0,5.
+
+        """
+        return Fraction(personnage.poids / personnage.poids_max)
