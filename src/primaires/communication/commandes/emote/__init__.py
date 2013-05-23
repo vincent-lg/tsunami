3b43f733f9628f9926acd47dcad1e89d6d2df7f7
Kredh <kredh@free.fr>
2013-05-23 12:53:01 +0200

    Ajout de la paralysie

--- src/primaires/communication/commandes/emote/__init__.py
+++ src/primaires/communication/commandes/emote/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,11 +38,11 @@ from primaires.format.fonctions import echapper_accolades
 from primaires.interpreteur.commande.commande import Commande
 
 class CmdEmote(Commande):
-    
+
     """Commande 'emote'.
-    
+
     """
-    
+
     def __init__(self):
         """Constructeur de la commande"""
         Commande.__init__(self, "emote", "emote")
@@ -55,9 +55,10 @@ class CmdEmote(Commande):
             "la salle vous verront. Par exemple, vous pouvez faire " \
             "|ent|emote sifflote un air mélodieux|ff| ou |ent|emote " \
             "sourit|ff|."
-    
+
     def interpreter(self, personnage, dic_masques):
         """Interprétation de la commande"""
+        personnage.agir("geste")
         message = dic_masques["message"].message
         message = message.rstrip(" \n")
         if not message[-1] in ponctuations_finales:
--- src/primaires/perso/__init__.py
+++ src/primaires/perso/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -52,17 +52,17 @@ from .templates.etat import Etat
 from .templates.position import Position
 
 class Module(BaseModule):
-    
+
     """Module gérant la classe Personnage qui sera héritée pour construire
     des joueurs et PNJs. Les mécanismes propres au personnage (c'est-à-dire
     indépendants de la connexion et liés à l'univers) seront gérés ici.
-    
+
     En revanche, les contextes de connexion ou de création d'un personnage
     ne se trouvent pas ici (il s'agit d'informations propres à un joueur, non
     à un PNJ).
-    
+
     """
-    
+
     def __init__(self, importeur):
         """Constructeur du module"""
         BaseModule.__init__(self, importeur, "perso", "primaire")
@@ -78,12 +78,12 @@ class Module(BaseModule):
         self.talents = {}
         self.etats = {}
         self.positions = {}
-    
+
     def config(self):
         """Méthode de configuration.
-        
+
         On récupère les fichiers de configuration correspondant au module.
-        
+
         """
         self.cfg_stats = conf_stats = type(self.importeur).anaconf.get_config(
                 "stats", "perso/stats.cfg", "modele stats", cfg_stats)
@@ -93,19 +93,19 @@ class Module(BaseModule):
             "SM":SM,
             "SEM":SEM,
         })
-        
+
         self.modele_stats = Stats()
-        
+
         self.cfg_niveaux = type(self.importeur).anaconf.get_config(
                 "niveaux", "perso/niveaux.cfg", "modele niveaux", cfg_niveaux)
-        
+
         self.cfg_talents = type(self.importeur).anaconf.get_config(
                 "talents", "perso/talents.cfg", "modele talents", cfg_talents)
-        
+
         self.ajouter_niveau("art_pisteur", "art du pisteur")
-        
+
         BaseModule.config(self)
-    
+
     def init(self):
         """Initialisation du module"""
         # On construit le niveau
@@ -120,21 +120,21 @@ class Module(BaseModule):
         niveaux.stats_entrainables = self.cfg_stats.entrainables
         niveaux.calculer_grille()
         self.gen_niveaux = niveaux
-        
+
         # On récupère les squelettes
         squelettes = self.importeur.supenr.charger_groupe(Squelette)
         for squelette in squelettes:
             self.ajouter_squelette(squelette)
-        
+
         # On récupère les races
         races = self.importeur.supenr.charger_groupe(Race)
         for race in races:
             self.ajouter_race(race)
-        
+
         # Positions
         self.ajouter_position("assis", "est assis", "est assise")
         self.ajouter_position("allonge", "est allongé", "est allongée")
-        
+
         # Ajout des états (assis et allongé)
         assis = self.ajouter_etat("assis")
         assis.msg_refus = "Vous êtes assis."
@@ -145,21 +145,25 @@ class Module(BaseModule):
         allonge.msg_refus = "Vous êtes allongé."
         allonge.msg_visible = "est allongé là"
         allonge.act_autorisees = ["regarder", "parler", "ingerer", "lever"]
-        
+
         mort = self.ajouter_etat("mort")
         mort.msg_refus = "Vous êtes inconscient."
         mort.msg_visible = "est inconscient ici"
-        
+        paralyse = self.ajouter_etat("paralyse")
+        paralyse.msg_refus = "Vous ne pouvez bouger un muscle."
+        paralyse.msg_visible = "se tient, rigide, à cet endroit"
+        paralyse.peut_etre_attaque = False
+
         entraine = self.ajouter_etat("entrainer")
         entraine.msg_refus = "Vous êtes en train de vous entraîner."
         entraine.msg_visible = "s'entraîne ici"
         entraine.act_autorisees = ["regarder", "parler"]
-        
+
         self.ajouter_talent("escalade", "escalade", "survie", 0.31)
         self.ajouter_talent("nage", "nage", "survie", 0.25)
-        
+
         BaseModule.init(self)
-    
+
     def ajouter_commandes(self):
         """Ajout des commandes dans l'interpréteur"""
         self.commandes = [
@@ -178,14 +182,14 @@ class Module(BaseModule):
             commandes.sklist.CmdSklist(),
             commandes.talents.CmdTalents(),
         ]
-        
+
         for cmd in self.commandes:
             self.importeur.interpreteur.ajouter_commande(cmd)
-        
+
         # Ajout des éditeurs
         self.importeur.interpreteur.ajouter_editeur(EdtRaedit)
         self.importeur.interpreteur.ajouter_editeur(EdtSkedit)
-    
+
     def preparer(self):
         """Préparation des personnages."""
         personnages = list(importeur.joueur.joueurs.values()) + list(
@@ -194,42 +198,42 @@ class Module(BaseModule):
             if personnage.salle and personnage.salle.nom_terrain == \
                     "subaquatique" and not personnage.est_immortel():
                 personnage.plonger()
-    
+
     def get_niveau_par_nom(self, nom):
         """Retourne le niveau dont le nom est donn├®."""
         nom = supprimer_accents(nom).lower()
         for niveau in self.niveaux.values():
             if supprimer_accents(niveau.nom).lower() == nom:
                 return niveau
-    
+
         raise ValueError("niveau inconnu {}".format(nom))
-    
+
     def creer_squelette(self, cle):
         """Création d'un squelette"""
         squelette = Squelette(cle)
         self.ajouter_squelette(squelette)
         return squelette
-    
+
     def ajouter_squelette(self, squelette):
         """Ajoute le squelette aux squelettes existants"""
         self.squelettes[squelette.cle] = squelette
-    
+
     def supprimer_squelette(self, cle):
         """Supprime le squelette existant"""
         squelette = self.squelettes[cle]
         del self.squelettes[cle]
         squelette.detruire()
-    
+
     def creer_race(self, nom):
         """Crée la race du nom indiqué"""
         race = Race(nom)
         self.ajouter_race(race)
         return race
-    
+
     def ajouter_race(self, race):
         """Ajout de la race au dictionnaire des races existantes"""
         self.races.append(race)
-    
+
     def supprimer_race(self, nom):
         """Suppression de la race 'nom'"""
         race = None
@@ -238,37 +242,37 @@ class Module(BaseModule):
             if t_race.nom.lower() == nom.lower():
                 race = t_race
                 indice = i
-        
+
         if indice is None:
             raise KeyError("ce nom de race est introuvable")
-        
+
         del self.races[indice]
         race.detruire()
-    
+
     def race_est_utilisee(self, race):
         """Contrôle si la race est déjà utilisée ou non.
-        
+
         La race peut être utilisée :
         -   par un joueur
         -   par un prototype de PNJ
-        
+
         """
         a_tester = list(self.importeur.connex.joueurs)
         a_tester += list(self.importeur.pnj.prototypes.values())
         for test in a_tester:
             if test.race is race:
                 return True
-        
+
         return False
-    
+
     def stats_symboles(self):
         """Retourne un tuple nommé contenant les stats et leur symbole.
-        
+
         Par exemple :
         >>> nt = importeur.perso.stats_symboles()
         >>> nt.force
         'f'
-        
+
         """
         NTStats = namedtuple("NTStats",
                 [stat.nom for stat in self.modele_stats])
@@ -276,34 +280,34 @@ class Module(BaseModule):
                 for stat in self.modele_stats))
         ntstats = NTStats(**stats_symboles)
         return ntstats
-    
+
     def ajouter_niveau(self, cle, nom):
         """Ajoute un niveau au dictionnaire des niveaux."""
         if cle in self.niveaux:
             raise ValueError("la clé {} est déjà utilisée comme clé " \
                     "de niveau".format(repr(cle)))
-        
+
         niveau = Niveau(cle, nom)
         self.niveaux[cle] = niveau
-    
+
     def ajouter_talent(self, cle, nom, niveau, difficulte):
         """Ajoute un talent."""
         if cle in self.talents:
             raise ValueError("un talent de clé {} existe déjà".format(cle))
-        
+
         talent = Talent(self.niveaux, cle, nom, niveau, difficulte)
         self.talents[cle] = talent
-    
+
     def ajouter_etat(self, cle):
         """Ajoute un état dans le dictionnaire."""
         if cle in self.etats:
             raise ValueError("l'état {} existe déjà".format(cle))
-        
+
         etat = Etat(cle)
         self.etats[cle] = etat
-        
+
         return etat
-    
+
     def ajouter_position(self, cle, etat_m, etat_f):
         """Ajoute une position."""
         position = Position(cle, etat_m, etat_f)
