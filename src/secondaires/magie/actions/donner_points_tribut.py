300a8b0581f44442045a58429f64842847a57321
kredh <kredh@free.fr>
2013-06-06 08:44:28 +0200

    Ajout d'une fonction scripting pour donner des points de tribut à un personnage

---
+++ src/secondaires/magie/actions/donner_points_tribut.py
@@ -0,0 +1,61 @@
+﻿# -*-coding:Utf-8 -*
+
+# Copyright (c) 2013 LE GOFF Vincent
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Fichier contenant l'action donner_points_tribut."""
+
+from primaires.scripting.action import Action
+from primaires.scripting.instruction import ErreurExecution
+
+class ClasseAction(Action):
+
+    """Donne des points de tribut au personnage."""
+
+    @classmethod
+    def init_types(cls):
+        cls.ajouter_types(cls.donner_tribut, "Personnage", "Fraction")
+
+    @staticmethod
+    def donner_tribut(personnage, nb_points):
+        """Donne des points de tribut au personnage.
+
+        Les paramètres à préciser sont :
+
+          * personnage - le personnage à qui l'on souhaite donner des points
+          * nb_points : le nombre de points (supérieur à 0) à donner.
+
+        Vous pouvez donner des points de tribut à des PNJ, même si cela
+        semble moins utile à plus long terme.
+
+        """
+        points = int(nb_points)
+        if points <= 0:
+            raise ErreurExecution("nombre de points de tribut négatif ou nul")
+
+        personnage.points_tribut += points
