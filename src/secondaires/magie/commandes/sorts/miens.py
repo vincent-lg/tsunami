10405606999d9a561116a20f9628446ea346b96f
Kredh <kredh@free.fr>
2013-06-10 13:35:39 +0200

    Ajout des sorts verrouillés

--- src/secondaires/magie/commandes/sorts/miens.py
+++ src/secondaires/magie/commandes/sorts/miens.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2012 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   mine of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -33,11 +33,11 @@
 from primaires.interpreteur.masque.parametre import Parametre
 
 class PrmMiens(Parametre):
-    
+
     """Commande 'sorts miens'.
-    
+
     """
-    
+
     def __init__(self):
         """Constructeur de la commande"""
         Parametre.__init__(self, "miens", "mine")
@@ -45,23 +45,26 @@ class PrmMiens(Parametre):
         self.aide_longue = \
             "Cette commande vous permet de consulter votre niveau actuel " \
             "dans chacun des sorts que vous connaissez."
-    
+
     def interpreter(self, personnage, dic_masques):
         """Méthode d'interprétation de commande"""
         ret = "Vos sorts :\n"
         sorts = {}
-        for cle, niveau in tuple(personnage.sorts.items()):
+        for cle, niveau in personnage.sorts.items():
             try:
                 sort = importeur.magie.sorts[cle]
             except KeyError:
-                del personnage.sorts[cle]
-            
-            sorts[sort.nom] = niveau
-        
+                continue
+
+            if cle not in personnage.sorts_verrouilles:
+                sorts[sort.nom] = niveau
+            else:
+                sorts[sort.nom + " (verrouillé)"] = niveau
+
         for nom, niveau in sorted(sorts.items()):
-            ret += "\n  {:<20} - {:>3}%".format(nom, niveau)
-        
+            ret += "\n  {:<35} - {:>3}%".format(nom, niveau)
+
         if not sorts:
             ret = "Vous ne connaissez aucun sort... pour l'instant."
-        
+
         personnage << ret
