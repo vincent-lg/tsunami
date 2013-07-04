603a82dfdb5e1ee030338746d1a3936335c81ab5
kredh <kredh@free.fr>
2013-07-04 08:55:10 +0200

    Ajout du format pour l'export d'aide 'pgsql' (base encore)

--- src/secondaires/exportaide/__init__.py
+++ src/secondaires/exportaide/__init__.py
@@ -38,6 +38,11 @@ sujets d'aide) dans différents formats.
 from abstraits.module import *
 
 from secondaires.exportaide.config import CFG_FORMAT
+from secondaires.exportaide.formats.pgsql.format import PGFormat
+
+formats = {
+    "pgsql": PGFormat,
+}
 
 class Module(BaseModule):
 
@@ -51,6 +56,8 @@ class Module(BaseModule):
         """Constructeur du module"""
         BaseModule.__init__(self, importeur, "exportaide", "secondaire")
         self.format = None
+        self.exp_logger = type(importeur).man_logs.creer_logger(
+                "exportaide", "exportaide", "exportaide.log")
 
     def config(self):
         """Méthode de configuration.
@@ -79,5 +86,24 @@ class Module(BaseModule):
         """
         nom_format = self.cfg.nom_format
         if nom_format is None:
-            raise ValueError(
+            self.exp_logger.fatal(
                     "le format spécifié dans la configuration est indéfini")
+            return
+
+        if nom_format not in formats:
+            self.exp_logger.fatal("Format d'export {} inconnu".format(
+                    repr(nom_format)))
+            return
+
+        format = formats[nom_format]()
+        if not format.peut_tourner:
+            self.format.exp_logger.fatal("Le format {} sélectionné ne peut " \
+                    "pas s'exécuter, probablement à cause de modules " \
+                    "tierses manquants".format(repr(nom_format)))
+            return
+
+        format.config()
+        if not format.init():
+            self.exp_logger.fatal("Initialisation du format {} échouée. " \
+                    "Export annulé.".format(repr(nom_format)))
+            return
