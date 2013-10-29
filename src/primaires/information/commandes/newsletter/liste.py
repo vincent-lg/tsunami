da679bd2b97ce3f7e020c316e500afdcf8f75fa9
Kredh <kredh@free.fr>
2013-10-29 08:30:28 +0100

    Correction d'un bug sur l'affichage des newsletters

--- src/primaires/information/commandes/newsletter/liste.py
+++ src/primaires/information/commandes/newsletter/liste.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2012 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -33,11 +33,11 @@
 from primaires.interpreteur.masque.parametre import Parametre
 
 class PrmListe(Parametre):
-    
+
     """Commande 'newsletter liste'.
-    
+
     """
-    
+
     def __init__(self):
         """Constructeur du paramètre"""
         Parametre.__init__(self, "liste", "list")
@@ -45,30 +45,30 @@ class PrmListe(Parametre):
         self.aide_longue = \
             "Cette commande liste les news letters existantes ainsi que " \
             "leur statut."
-    
+
     def interpreter(self, personnage, dic_masques):
         """Interprétation du paramètre."""
         newsletters = list(importeur.information.newsletters)
         if len(newsletters) == 0:
             personnage << "Aucune news letter n'a été créé pour l'instant."
             return
-        
+
         lignes = [
             "+----|------------+------------+---------------------------|",
             "| ID | Date       | Statut     | Sujet                     |",
         ]
         lignes.append(lignes[0])
-        nid = len(newsletters)
+        nid = 1
         for newsletter in newsletters:
             date = str(newsletter.date_creation.date())
             statut = newsletter.statut
             sujet = newsletter.sujet
             if len(sujet) > 25:
                 sujet = sujet[:22] + "..."
-            
+
             lignes.append("| {:>2} | {} | {:<10} | {:<25} |".format(
                     nid, date, statut, sujet))
-            nid -= 1
-        
+            nid += 1
+
         lignes.append(lignes[0])
         personnage << "\n".join(lignes)
