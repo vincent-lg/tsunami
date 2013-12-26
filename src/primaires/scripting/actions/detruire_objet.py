c2ae242a3f662328d0922ffdde5f544b287ef744
Kredh <kredh@free.fr>
2013-12-26 08:20:06 +0100

    Petite correction de detruire_objet dans le scripting

--- src/primaires/scripting/actions/detruire_objet.py
+++ src/primaires/scripting/actions/detruire_objet.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2012 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -33,24 +33,40 @@
 from primaires.scripting.action import Action
 
 class ClasseAction(Action):
-    
+
     """Détruit un objet."""
-    
+
     @classmethod
     def init_types(cls):
+        cls.ajouter_types(cls.detruire_objet, "Objet")
         cls.ajouter_types(cls.detruire_objet_personnage, "Personnage", "str")
         cls.ajouter_types(cls.detruire_objet_personnage, "Personnage", "str",
                 "Fraction")
-    
+
+    @staticmethod
+    def detruire_objet(objet):
+        """Détruit l'objet spécifié.
+
+        L'objet peu être n'importe où (possédé par un personnage, posé sur
+        le sol ou même existant dans les limbes) mais il faut préciser
+        l'objet, pas son identifiant.
+
+        Exemple d'utilisation :
+
+          detruire_objet objet
+
+        """
+        importeur.objet.supprimer_objet(objet.identifiant)
+
     @staticmethod
     def detruire_objet_personnage(personnage, cle_objet, a_detruire=1):
         """Détruit le premier objet indiqué détenu par le personnage.
-        
+
         L'objet détenu peut être :
           * Tenu par le personnage
           * Equipé par le personnage
           * Contenu dans un conteneur équipé par le personnage
-        
+
         """
         a_detruire_o = a_detruire
         det_qtt = 0
@@ -62,7 +78,7 @@ class ClasseAction(Action):
                 conteneur.retirer(objet, qtt)
                 if hasattr(objet, "identifiant"):
                     importeur.objet.supprimer_objet(objet.identifiant)
-                
+
                 a_detruire -= qtt
                 det_qtt += qtt
                 if det_qtt >= a_detruire_o:
