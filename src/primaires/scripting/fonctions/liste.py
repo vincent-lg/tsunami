c84fa24179fb719010301d151bf25e6a5cdd2f26
Kredh <kredh@free.fr>
2014-01-25 17:47:27 +0100

    Ajoute de la fonction liste

---
+++ src/primaires/scripting/fonctions/liste.py
@@ -0,0 +1,69 @@
+﻿# -*-coding:Utf-8 -*
+
+# Copyright (c) 2014 LE GOFF Vincent
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Fichier contenant la fonction liste."""
+
+from primaires.scripting.fonction import Fonction
+from primaires.scripting.instruction import ErreurExecution
+
+class ClasseFonction(Fonction):
+
+    """Crée une liste.
+
+    Cette fonction permet de créer une liste. Elle peut ne prendre aucun argument ou bien un nombre indéfini.
+
+    Voici un exemple d'utilisation :
+
+      liste = liste(1, 2, 3)
+      pour chaque element dans liste:
+          dire personnage "Élément n° ${element}"
+      finsi
+
+    Ce script enverra au personnage :
+
+      Élément n° 1
+      Élément n° 2
+      Élément n° 3
+
+    Les éléments peuvent être de tous les types supportés par le scripting :
+    chaînes, nombres, fonctions ou même des objets spécifiques à l'univers,
+    comme des PNJ.
+
+    """
+
+    verifier = False
+    @classmethod
+    def init_types(cls):
+        cls.ajouter_types(cls.liste)
+
+    @staticmethod
+    def liste(*elements):
+        """Retourne une liste contenant les éléments."""
+        return list(elements)
