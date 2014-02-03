698d6a1e2f7e8322f6c7d5d37bd58929e8835ca8
Kredh <kredh@free.fr>
2014-02-03 15:04:28 +0100

    Suppression de l'erreur quand la mémoire a déjà été prévue

--- src/primaires/scripting/memoires.py
+++ src/primaires/scripting/memoires.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -37,67 +37,63 @@ from abstraits.obase import BaseObj, MetaBaseObj
 from .exceptions import ErreurScripting
 
 class Memoires(BaseObj):
-    
+
     """Classe enveloppe définissant les mémoires du scripting.
-    
+
     Les mémoires peuvent être liées à un PNJ, une salle, un objet, l'univers...
     Elles sont manipulées en scripting par l'action ecrire_memoire, et les
     fonctions memoire_existe et memoire.
-    
+
     """
-    
+
     enregistrer = True
     def __init__(self, parent=None):
         """Constructeur de la classe"""
         BaseObj.__init__(self)
         self._memoires = {}
         self._a_detruire = {}
-    
+
     def __getnewargs__(self):
         return ()
-    
+
     def __delitem__(self, cle):
         del self._memoires[cle]
 
     def __getitem__(self, cle):
         return self._memoires[cle]
-    
+
     def __setitem__(self, cle, valeur):
         self._memoires[cle] = valeur
-    
+
     def __contains__(self, valeur):
         return valeur in self._memoires
-    
+
     def nettoyer_memoire(self, cle, valeur):
         """Nettoie la mémoire à détruire.
-        
+
         Cette méthode est appelée quand on efface tout de suite une mémoire.
         Si la mémoire est effacée mais qu'elle est toujours programmée pour
         destruction, il va y avoir problème. On nettoie donc les mémoires à détruire.
-        
+
         """
         if cle in self._a_detruire and valeur in self._a_detruire[cle]:
             del self._a_detruire[cle][valeur]
         if cle in self._a_detruire and not self._a_detruire[cle]:
             del self._a_detruire[cle]
-    
+
     def programmer_destruction(self, cle, valeur, temps):
         """Programme la destruction de la mémoire.
-        
+
         Paramètres :
             cle -- la clé de la mémoire (souvent un objet, une salle, un PNJ...)
             valeur -- la valeur de la mémoire (c'est-à-dire le nom mémorisé)
             temps -- le temps en minutes
-        
+
         """
         if cle not in self or valeur not in self[cle]:
             raise ValueError("la mémoire {} n'existe pas dans {}".format(
                     valeur, cle))
-        
-        if cle in self._a_detruire and valeur in self._a_detruire[cle]:
-            raise ValueError("la destruction de la mémoire {} de {} est déjà " \
-                    "prévue".format(valeur, cle))
-        
+
         a_detruire = self._a_detruire.get(cle, {})
         a_detruire[valeur] = datetime.now() + timedelta(seconds=temps * 60)
         self._a_detruire[cle] = a_detruire
