8b6661b5d10ba825629894a130a401600b4143ae
Kredh <kredh@free.fr>
2014-02-11 08:25:37 +0100

    Correction d'un bug dans la commande oublier/forget

--- src/primaires/joueur/commandes/oublier/__init__.py
+++ src/primaires/joueur/commandes/oublier/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 DAVY Guillaume
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -34,11 +34,11 @@ from primaires.interpreteur.commande.commande import Commande
 from primaires.format.fonctions import contient
 
 class CmdOublier(Commande):
-    
+
     """Commande 'oublier'.
-    
+
     """
-    
+
     def __init__(self):
         """Constructeur de la commande"""
         Commande.__init__(self, "oublier", "forget")
@@ -53,7 +53,7 @@ class CmdOublier(Commande):
             "à chaque utilisation de cette commande limitera les points " \
             "d'apprentissage récupérés, sauf si vous avez une connaissance " \
             "du talent inférieure à 10|pc|."
-    
+
     def interpreter(self, personnage, dic_masques):
         """Méthode d'interprétation de commande"""
         nom_talent = dic_masques["message"].message
@@ -68,4 +68,4 @@ class CmdOublier(Commande):
                 personnage << "Vous avez oublié le talent {}.".format(tal.nom)
                 return
         personnage << "|err|Le talent '{}' est introuvable.|ff|".format(
-                nom_sort)
+                nom_talent)
