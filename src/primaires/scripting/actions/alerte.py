202575787c95075b8a7993f9598ce1610634693f
Kredh <kredh@free.fr>
2014-06-13 13:49:42 +0200

    Ajout de l'action scripting 'alerte' et la fonction 'est_en_combat'

---
+++ src/primaires/scripting/actions/alerte.py
@@ -0,0 +1,61 @@
+﻿# -*-coding:Utf-8 -*
+
+# Copyright (c) 2014 LE GOFF Vincent
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# * Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# * Neither the name of the copyright holder nor the names of its contributors
+#   may be used to endorse or promote products derived from this software
+#   without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+
+"""Fichier contenant l'action alerte."""
+
+from primaires.scripting.action import Action
+from primaires.scripting.instruction import ErreurExecution
+
+class ClasseAction(Action):
+
+    """Envoie une alerte scripting."""
+
+    @classmethod
+    def init_types(cls):
+        cls.ajouter_types(cls.alerte, "str")
+
+    @staticmethod
+    def alerte(message):
+        """Envoie une alerte scripting.
+
+        Paramètres à préciser :
+
+          * message : le message d'erreur à indiquer
+
+        Exemple d'utilisation :
+
+          alerte "Ce PNJ est introuvable."
+
+        Cette action scripting est surtout utile pour renforcer la stabilité
+        d'un script (ou, souvent, d'un bloc d'évènement) appelé dans des
+        situations diverses.
+
+        """
+        raise ErreurExecution(message)
