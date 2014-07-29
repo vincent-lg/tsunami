57038a98c4e2a4d7a72e8dae52bb83b9e20c2631
Kredh <kredh@free.fr>
2014-07-29 21:14:44 +0200

    Correction d'un bug dans les contrôles de navigation

--- src/secondaires/navigation/equipage/controle.py
+++ src/secondaires/navigation/equipage/controle.py
@@ -84,7 +84,7 @@ class Controle(BaseObj, metaclass=MetaControle):
         self.arguments = args
 
     def __getnewargs__(self):
-        arguments = (self.equipage, ) + self.arguments
+        arguments = (None, ) + self.arguments
         return arguments
 
     def __repr__(self):
