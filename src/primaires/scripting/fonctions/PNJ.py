a3e9e99e6e89d34a39d3b35b346c86982e7f1501
Kredh <kredh@free.fr>
2014-09-03 13:50:18 -0700

    Ajout d'une extension à la fonction PNJ pour rechercher tous les PNJ d'un prototype

--- src/primaires/scripting/fonctions/PNJ.py
+++ src/primaires/scripting/fonctions/PNJ.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2012 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -31,20 +31,57 @@
 """Fichier contenant la fonction PNJ."""
 
 from primaires.scripting.fonction import Fonction
+from primaires.scripting.instruction import ErreurExecution
 
 class ClasseFonction(Fonction):
-    
-    """Retourne tous les PNJ présents dans la salle."""
-    
+
+    """Retourne tous les PNJ présents."""
+
     @classmethod
     def init_types(cls):
         cls.ajouter_types(cls.PNJ_salle, "Salle")
-    
+        cls.ajouter_types(cls.tous_PNJ, "str")
+
     @staticmethod
     def PNJ_salle(salle):
         """Retourne tous les PNJ présents dans la salle.
-        
+
         Cette fonction retourne les PNJ présents (pas les joueurs).
-        
+
         """
         return salle.PNJ
+
+    @staticmethod
+    def tous_PNJ(cle_prototype):
+        """Retourne tous les PNJ d'un prototype.
+
+        Paramètres à préciser :
+
+          * cle_prototype : la clé du prototype de PNJ
+
+        Vous devez préciser la clé du prototype sous la
+        forme d'une chaîne. Une liste contenant tous les
+        PNJ du prototype, peu importe l'endroit où ils
+        se trouvent, sera retournée. La liste sera
+        vide si le prototype n'a aucun PNJ créé
+        actuellement. Vous pouvez partir du principe que
+        tous les PNJ retournés sont vivants (ceux morts
+        sont automatiquement supprimés).
+
+        Utilisez la fonction 'salle' pour connaître la
+        salle dans laquelle un PNJ se trouve.
+
+        Exemple d'utilisation :
+
+          pour chaque pnj dans PNJ("marchand_myr"):
+              salle = salle(pnj)
+              # salle contient la salle du PNJ parcouru
+
+        """
+        try:
+            prototype = importeur.pnj.prototypes[cle_prototype.lower()]
+        except KeyError:
+            raise ErreurExecution("prototype inconnu {}".format(
+                    repr(cle_prototype)))
+
+        return list(prototype.pnj)
