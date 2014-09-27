5f03d2970bc10f7a1b7b12cdae98f7d6fc0b5010
kredh <kredh@free.fr>
2014-09-27 20:28:16 +0200

    Ajout de documentation dans les modules secondaires

--- src/secondaires/__init__.py
+++ src/secondaires/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -49,7 +49,12 @@ NOTE: les modules primaires et secondaires ne doivent pas porter de noms
 identiques.
 
 Voici un résumé des modules primaires existants :
+-   auberge     Module gérant les auberges
+-   botanique   Module gérant les végétaux récoltables
+-   calendrier  Module gérant un calendrier d'évènements
 -   cuisine     Module chargé de la cuisine
+-   exportaide  Module exportant l'aide dans un autre format
+-   familier    Module gérant les familiers et montures
 -   jeux        Module traitant les jeux et plateaux de jeu
 -   magie       Module contenant la magie et les sorts
 -   navigation  Module gérant la navigation, navires, équipages
