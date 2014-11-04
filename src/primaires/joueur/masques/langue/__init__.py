168e95415c69d8edb2f74c40425958c2ec84ea1d
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2014-11-03 20:06:00 -0800

    Correction esthétique dans les options de langue

--- src/primaires/joueur/masques/langue/__init__.py
+++ src/primaires/joueur/masques/langue/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -39,35 +39,35 @@ from primaires.format.fonctions import *
 LANGUES = ("anglais", "francais")
 
 class Langue(Masque):
-    
+
     """Masque <nom_langue>.
     On attend un nom de langue en paramètre.
-    
+
     """
-    
+
     nom = "nom_langue"
     nom_complet = "langue disponible"
-    
+
     def init(self):
         """Initialisation des attributs"""
         self.langue = ""
-    
+
     def repartir(self, personnage, masques, commande):
         """Répartition du masque."""
         langue = liste_vers_chaine(commande)
-        
+
         if not langue:
             raise ErreurValidation( \
-                "|err|Précisez une langue disponible (|ff||ent|anglais|ff| " \
-                "|err| ou |ff||ent|français|ff||err|).|ff|")
-        
+                "|err|Précisez une langue disponible (|ent|anglais|ff| " \
+                "|err| ou |ent|français|err|).|ff|")
+
         langue = langue.split(" ")[0].lower()
         langue = supprimer_accents(langue)
         self.a_interpreter = langue
         commande[:] = commande[len(langue):]
         masques.append(self)
         return True
-    
+
     def valider(self, personnage, dic_masques):
         """Validation du masque"""
         Masque.valider(self, personnage, dic_masques)
@@ -76,7 +76,7 @@ class Langue(Masque):
         if not langue in LANGUES:
             raise ErreurValidation(
                 "|err|La langue précisée n'est pas disponible.|ff|")
-        
+
         self.langue = langue
-        
+
         return True
