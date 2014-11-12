da8a612d3e858d2fa933df97b358d7ecf867e2c0
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2014-11-11 20:55:05 -0800

    Ajout d'une extension à l'action scripting ajouter pour ajouter à l'inventaire d'un magasin

--- src/primaires/scripting/actions/ajouter.py
+++ src/primaires/scripting/actions/ajouter.py
@@ -31,9 +31,68 @@
 """Fichier contenant l'action ajouter"""
 
 from primaires.scripting.actions.ajouter_a_liste import ClasseAction as CA
+from primaires.scripting.instruction import ErreurExecution
 
 class ClasseAction(CA):
 
-    """Ajoute un élément à une liste."""
+    """Ajoute un élément."""
 
-    pass
+    @classmethod
+    def init_types(cls):
+        cls.ajouter_types(cls.ajouter_a_liste, "object", "list")
+        cls.ajouter_types(cls.ajouter_magasin, "Salle", "Fraction",
+                "str", "str")
+
+    @staticmethod
+    def ajouter_magasin(salle, quantite, service, cle):
+        """Ajoute un service à l'inventaire du magasin.
+
+        Paramètres à préciser :
+
+          * salle : la salle contenant le magasin
+          * quantite : la quantité de services à ajouter
+          * service : le type de service (objet, potion, navire...)
+          * cle : la clé du service (la clé de l'objet par exemple)
+
+        Cette action s'utilise de façon très similaire à l'option
+        /s dans l'éditeur de magasin d'une salle. Le service et
+        la clé ont besoin d'être utilisés conjointement : le service
+        le plus simple est 'objet' qui permet d'ajouter un ou plusieurs
+        objets à la vente dans le magasin. Mais un magasin peut vendre
+        d'autres services, comme des potions, de la nourriture, des
+        navires, des familiers, des matelots, ainsi de suite. Pour
+        chaque type de service, le nom à entrer est différent. Notez
+        cependant qu'à la différence de l'option /s dans l'éditeur
+        du magasin d'une salle, cette action ajoute le service directement
+        dans l'inventaire du magasin, ce qui veut dire qu'il sera
+        proposé en vente. En revanche, cela veut aussi dire qu'au
+        renouvellement du magasin, qui arrive généralement au moins
+        une fois par jour IG, le service ajouté disparaîtra.
+
+        Exemples d'utilisation :
+
+          salle = salle("zone:mnemo")
+          # Ajoute 5 objets 'chausse_laine'
+          ajouter salle 5 "objet" "chaussette_laine"
+          # Ajoute 2 navires 'barque_peche'
+          ajouter salle 2 "navire" "barque_peche"
+          # Ajoute 4 familiers 'cheval_blanc'
+          ajouter salle 4 "familier" "cheval_blanc"
+
+        """
+        quantite = int(quantite)
+        if quantite < 1:
+            raise ErreurExecution("Quantité {} négative ou nulle".format(
+                    quantite))
+
+        if service not in importeur.commerce.types_services:
+            raise ErreurExecution("Type de service {} inconnu".format(
+                    repr(service)))
+
+        objets = importeur.commerce.types_services[service]
+        if cle not in objets:
+            raise ErreurExecution("le produit {} de service {} n'a " \
+                    "pas pu être trouvé".format(repr(cle), repr(service)))
+
+        service = objets[cle]
+        salle.magasin.ajouter_inventaire(service, quantite)
