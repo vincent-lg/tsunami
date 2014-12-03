c72851185c66270146e2b58d15f3984fd177f887
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2014-12-02 16:36:38 -0800

    Extension de la fonction scripting 'equipe' pour inclure les types d'objet

--- src/primaires/scripting/fonctions/equipe.py
+++ src/primaires/scripting/fonctions/equipe.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -33,19 +33,53 @@
 from primaires.scripting.fonction import Fonction
 
 class ClasseFonction(Fonction):
-    
+
     """Teste si un personnage équipe un objet."""
-    
+
     @classmethod
     def init_types(cls):
         cls.ajouter_types(cls.possede_proto, "Personnage", "str")
-    
+
     @staticmethod
     def possede_proto(personnage, prototype):
         """Retourne vrai si le personnage équipe ce prototype, faux sinon.
-        
+
         Le personnage peut équiper un ou plusieurs objets modelés sur le
-        prototype.
-        
+        prototype. Vous pouvez également tester si un personnage
+        équipe un type en précisant un signe '+' avant le nom du
+        type (voir les exemples plus bas). Cette fonction retourne
+        une valeur nulle ou le premier objet trouvé ce qui permet
+        de capturer l'objet indiqué, si nécessaire pour après (là
+        encore, voir les exemples ci-dessous).
+
+        Paramètres à préciser :
+
+          * personnage : le personnage à tester
+          * prototype : la clé du prototype ou le nom du type d'objet
+
+        Exemples d'utilisation :
+
+          # Teste si le personnage équipe l'objet 'sabre_fer'
+          si equipe(personnage, "sabre_fer"):
+          # Ou le capture
+          sabre = equipe(personnage, "sabre_fer")
+          # On peut faire ensuite si sabre: pour vérifier que le sabre
+          # de fer a été trouvé équipé par le personnage
+          # Teste si le personnage équipe une arme
+          si equipe(personnage, "+arme"):
+          # Ou, là encore
+          arme = equipe(personnage, "+arme")
+          si arme:
+            # ...
+
         """
-        return prototype in [o.cle for o in personnage.equipement.equipes]
+        nom_type = prototype[1:] if prototype.startswith("+") else ""
+        for o in personnage.equipement.equipes:
+            if nom_type:
+                if o.est_de_type(nom_type):
+                    return o
+            else:
+                if o.cle == prototype:
+                    return o
+
+        return None
