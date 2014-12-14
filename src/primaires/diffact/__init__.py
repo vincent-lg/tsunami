9e55c394a7f234fa58f92ce81c0fc3e959ef71d6
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2014-12-13 22:26:43 -0800

    Correction d'une erreur dans les actions différées

--- src/primaires/diffact/__init__.py
+++ src/primaires/diffact/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010 LE GOFF Vincent
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -36,15 +36,15 @@ from abstraits.module import *
 from .action_differee import ActionDifferee
 
 class Module(BaseModule):
-    
+
     """Cette classe contient les informations du module primaire diffact.
-    
+
     Ce module permet de gérer des actions différés.
     Typiquement, quand un module primaire ou secondaire doit se mettre en pause
     pendant un certain temps, il ne peut pas paralyser la boucle synchro
     du projet. La solution la plus simple est de créer une action différée
     qui s'exécutera dans un temps fixé à l'avance.
-    
+
     Exemples :
     -   si une commande doit faire une pause pendant 3 secondes, au moment
         de la pause, la commande s'arrête et crée une action différée
@@ -56,7 +56,7 @@ class Module(BaseModule):
         une action différée. La méthode qui sera exécutée par l'action en
         question devra recréer une action différée devant s'exécuter 60
         secondes plus tard.
-    
+
     Concrètement, une action différée possède une référence vers
     une fonction ou méthode, et une liste de paramètres. Un temps d'échéance,
     précisé sous la forme d'un timestamp, permet de savoir quand appeler
@@ -65,13 +65,13 @@ class Module(BaseModule):
     d'échéance. Si une action est arrivée à son terme, on exécute la fonction
     en lui passant ses paramètres. On la supprime bien entendu de la liste
     des actions en attente.
-    
+
     Les actions différées étant contrôlées à chaque tour de boucle synchro,
     il faut prévoir un retard d'exécution maximum à peu près équivalent au
     temps moyen du Watch Dog.
-    
+
     """
-    
+
     def __init__(self, importeur):
         """Constructeur du module"""
         BaseModule.__init__(self, importeur, "diffact", "primaire")
@@ -79,16 +79,16 @@ class Module(BaseModule):
         self.ordre_actions = [] # [nom_action]
         self.logger = type(self.importeur).man_logs.creer_logger("diffact", \
                 "diffact")
-    
+
     def boucle(self):
         """Redéfinition de la méthode boucle du Module.
-        
+
         Cette méthode est appelée pour chaque module, à chaque tour de boucle
         temps réel.
-        
+
         """
         self.mettre_a_jour_actions()
-    
+
     def ajouter_action(self, nom_action, tps, ref_fonc, *args, **kwargs):
         """Cette méthode permet d'ajouter une action différée à la liste
         de celles en attente. On précise :
@@ -97,31 +97,31 @@ class Module(BaseModule):
         -   la référence vers la fonction ou la méthode à exécuter
         -   les paramètres non nommés organisés en tuple
         -   les paramètres nommés organisés dans un dictionnaire
-        
+
         """
         if nom_action in self.ordre_actions:
             self.logger.warning("L'action différée {} existe déjà. " \
                     "L'ancienne sera écrasée.".format(nom_action))
             self.retirer_action(nom_action)
-            
+
         action = ActionDifferee(nom_action, tps, ref_fonc, *args, **kwargs)
         self.actions[nom_action] = action
         # On ordonne les actions correctement
         self.ordre_actions = [nom for nom in self.ordre_actions \
             if self.actions[nom] <= action] + [nom_action] + \
             [nom for nom in self.ordre_actions if self.actions[nom] > action]
-        
-        
-    
+
+
+
     def retirer_action(self, nom, warning=True):
         """Méthode permettant de retirer une action différée de la liste de
         celles en attente.
-        
+
         """
         if not nom in self.ordre_actions:
             if not warning:
                 return
-            
+
             self.logger.warning("L'action différée {0} devant être " \
                     "supprimée n'existe pas".format(nom))
         else:
@@ -133,14 +133,17 @@ class Module(BaseModule):
         attente d'être exécutées. Elle parcourt la liste des actions en
         attente et exécute celle qui doivent l'être, tout en les supprimant
         de la liste.
-        
+
         """
         for nom in self.ordre_actions:
-            action = self.actions[nom]
-            if action.doit_exec():
+            action = self.actions.get(nom)
+            if action is None or action.doit_exec():
                 # On la supprime avant toute chose
                 self.retirer_action(nom)
                 # On l'exécute ensuite
+                if action is None:
+                    continue
+
                 try:
                     action.executer()
                 except Exception:
