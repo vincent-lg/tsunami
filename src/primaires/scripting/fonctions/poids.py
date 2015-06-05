acba0ee64651a58c149c9b43a2ef548ad8a9a5b4
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2015-06-05 05:23:55 +0200

    Extension de la fonction scripting 'poids' pour les prototypes d'objet

--- src/primaires/scripting/fonctions/poids.py
+++ src/primaires/scripting/fonctions/poids.py
@@ -42,6 +42,7 @@ class ClasseFonction(Fonction):
     @classmethod
     def init_types(cls):
         cls.ajouter_types(cls.poids, "Objet")
+        cls.ajouter_types(cls.poids_prototype, "str")
 
     @staticmethod
     def poids(objet):
@@ -63,3 +64,27 @@ class ClasseFonction(Fonction):
 
         """
         return Fraction(objet.poids)
+
+    @staticmethod
+    def poids_prototype(cle_prototype):
+        """Retourne le poids du prototype d'objet (en kilo).
+
+        Cette fonction retourne le poids unitaire du prototype d'objet,
+        tel que défini dans l'éditeur 'oedit'.
+
+        Paramètres à préciser:
+
+          * cle_prototype : la clé du prototype d'objet
+
+        Exemple d'utilisation :
+
+          poids = poids("pomme_rouge")
+
+        """
+        try:
+            prototype = importeur.objet.prototypes[cle_prototype]
+        except KeyError:
+            raise ErreurExecution("prototype d'objet inconnu {}".format(
+                    repr(cle_prototype)))
+
+        return Fraction(prototype.poids_unitaire)
