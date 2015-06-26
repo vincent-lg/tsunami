f776256dbde5f20006d21efcf26425185dbccd05
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2015-06-26 02:31:19 +0200

    Ajout de la base de l'algorithme de pathfinding entre routes

--- src/secondaires/route/__init__.py
+++ src/secondaires/route/__init__.py
@@ -30,6 +30,8 @@
 
 """Fichier contenant le module secondaire route."""
 
+from queue import PriorityQueue
+
 from abstraits.module import *
 from corps.fonctions import valider_cle
 from primaires.format.fonctions import format_nb
@@ -78,6 +80,9 @@ class Module(BaseModule):
         self.logger.info(format_nb(len(routes),
                 "{nb} route{s} récupérée{s}", fem=True))
 
+        self.importeur.hook["personnage:deplacer"].ajouter_evenement(
+                self.etendre_route)
+
         BaseModule.init(self)
 
     def creer_route(self, salle):
@@ -101,3 +106,123 @@ class Module(BaseModule):
 
         """
         self.routes.pop(ident).detruire()
+
+    def enregistrer(self, personnage):
+        """Enregistre un point.
+
+        Si le personnage ne dessine aucune route actuellement, en
+        crée une nouvelle. Sinon, complète la route.
+
+        """
+        route = self.en_cours.get(personnage)
+        if route is None:
+            route = self.creer_route(personnage.salle)
+            self.en_cours[personnage] = route
+        else:
+            del self.en_cours[personnage]
+            route.completer()
+
+        return route
+
+    def etendre_route(self, personnage, destination, sortie, endurance):
+        """Étend la route si besoin."""
+        if personnage in self.en_cours:
+            route = self.en_cours[personnage]
+            route.ajouter_sortie(destination)
+            print("On ajoute", destination, "à", route)
+
+    def trouver_chemin(self, origine, destination):
+        """Trouve le chemin le plus court entre deux salles.
+
+        Les deux salles doivent se trouver dans le complexe des*
+        routes (soit en tant que salle d'origine, de destination
+        ou de salles intermédiaires).
+
+        Algorithme utilisé : Dijkstra.
+        (http://www.redblobgames.com/pathfinding/a-star/introduction.html)
+
+        """
+        routes_origines = []
+        routes_destinations = []
+
+        for route in self.routes.values():
+            # La route contient-elle 'origine' ?
+            if route.origine is origine or origine in route.salles:
+                routes_origines.append(route)
+            # La route contient-elle 'destination' ?
+            if destination in route.salles:
+                routes_destinations.append(route)
+
+        # Si on n'a pu trouver de route, on lève une exception ValueError
+        if len(routes_origines) == 0:
+            raise ValueError("{} n'a pas pu être trouvé dans le " \
+                    "complexe des routes".format(origine))
+        if len(routes_destinations) == 0:
+            raise ValueError("{} n'a pas pu être trouvé dans le " \
+                    "complexe des routes".format(destination))
+
+        # Si une route directe existe entre les deux salles, la retourne
+        communes = [r for r in routes_origines if r in routes_destinations]
+        if communes:
+            route = communes[0]
+            indice_origine = route.salles.index(origine)
+            indice_destination = route.salles.index(destination)
+            return route.sorties[indice_origine + 1:indice_destination]
+
+        # On explore maintenant toutes les possibilités de liaison
+        couts = {}
+        for route_origine in routes_origines:
+            for route_destination in routes_destinations:
+                start = route_origine
+                goal = route_destination
+                frontier = PriorityQueue()
+                frontier.put(start, 0)
+                came_from = {}
+                cost_so_far = {}
+                came_from[start] = None
+                cost_so_far[start] = 0
+
+                while not frontier.empty():
+                    current = frontier.get()
+                    if current is goal:
+                        break
+
+                    for next in current.enfants:
+                        new_cost = cost_so_far[current] + len(next)
+                        if next not in cost_so_far or new_cost < cost_so_far[
+                            next]:
+                            cost_so_far[next] = new_cost
+                            priority = new_cost
+                            frontier.put(next, priority)
+                            came_from[next] = current
+
+                current = goal
+                path = [current]
+                while current is not start:
+                    current = came_from.get(current)
+                    if current is None:
+                        break
+
+                    path.append(current)
+
+                path.reverse()
+
+                # Vérifie la continuité de la route
+                if path[0] is route_origine and path[-1] is route_destination:
+                    cout = sum(len(r) for r in path)
+                    if origine in route_origine.salles:
+                        indice = route_origine.salles.index(origine)
+                        cout -= len(route_origine.salles) - 1 - indice
+
+                    indice = route_destination.salles.index(destination)
+                    cout -= len(route_destination.salles) - 1 - indice
+                    couts[cout] = path
+
+        # Cherche la route la plus courte
+        if couts:
+            courte = couts[min(couts)]
+        else:
+            raise ValueError("impossible de trouver la route " \
+                    "entre {} et {}".format(origine, destination))
+
+        return courte
--- src/secondaires/route/route.py
+++ src/secondaires/route/route.py
@@ -48,7 +48,7 @@ class Route(BaseObj):
         """Constructeur de la fiche."""
         BaseObj.__init__(self)
         self.origine = origine
-        self.destination = destination
+        self.destination = None
         self.salles = []
         self.sorties = []
         self._construire()
@@ -62,11 +62,45 @@ class Route(BaseObj):
     def __str__(self):
         return self.str_ident
 
+    def __len__(self):
+        return len(self.sorties)
+
+    def __eq__(self, route):
+        """Aide à la comparaison : ==."""
+        return len(self) == len(route)
+
+    def __ne__(self, route):
+        """Aide à la comparaison : !=."""
+        return len(self) != len(route)
+
+    def __ge__(self, route):
+        """Aide à la comparaison : <=."""
+        return len(self) <= len(route)
+
+    def __gt__(self, route):
+        """Aide à la comparaison : <."""
+        return len(self) < len(route)
+
+    def __le__(self, route):
+        """Aide à la comparaison : >=."""
+        return len(self) >= len(route)
+
+    def __lt__(self, route):
+        """Aide à la comparaison : >."""
+        return len(self) > len(route)
+
+    def __hash__(self):
+        return hash(self.ident)
+
+    @property
+    def finie(self):
+        return self.destination is not None
+
     @property
     def ident(self):
         origine = self.origine
         ident = []
-        if origine:
+        if self.origine:
             ident.append(self.origine.ident)
             if self.destination:
                 ident.append(self.destination.ident)
@@ -82,3 +116,74 @@ class Route(BaseObj):
             return "de {} à ...".format(ident[0])
         else:
             return "inconnue"
+
+    @property
+    def enfants(self):
+        """Retourne les routes enfants de self.
+
+        Ces routes sont celles dont l'origine est la destination*
+        de self (incluant donc la route contraire de self).
+
+        """
+        routes = list(importeur.route.routes.values())
+        return [r for r in routes if r.finie and r.origine is self.destination]
+
+    def ajouter_sortie(self, salle):
+        """Cherche à ajouter une nouvelle sortie.
+
+        La salle passée en paramètre est la salle à ajouter à la*
+        route en construction. La dernière salle est récupérée.*
+        Une sortie directe doit être trouvée entre la dernière
+        salle de la route et la nouvelle salle.
+
+        """
+        if self.finie:
+            raise ValueError("Cette route est déjà finie")
+
+        derniere = self.origine
+        if self.salles:
+            derniere = self.salles[-1]
+
+        # Cherche à récupérer la sortie entre 'derniere' et 'salle'
+        direction = ""
+        for sortie in derniere.sorties:
+            if sortie.salle_dest is salle:
+                direction = sortie.direction
+                break
+
+        if direction == "":
+            raise ValueError("Impossible de trouver une sortie entre " \
+                    "{} et {}".format(derniere, salle))
+
+        self.sorties.append(direction)
+        self.salles.append(salle)
+        return direction
+
+    def completer(self, reciproque=True):
+        """Complète la route.
+
+        La dernière salle entrée est utilisée.
+
+        Si le paramètre 'reciproque' est à True, la route en sens
+        contraire est également créée.
+
+        """
+        if self.finie:
+            raise ValueError("Cette route est déjà finie")
+
+        if not self.salles:
+            raise ValueError("Il n'y a pas de salles enregistrées " \
+                    "dans cette route")
+
+        del importeur.route.routes[self.ident]
+        derniere = self.salles[-1]
+        self.destination = derniere
+        importeur.route.ajouter_route(self)
+
+        if reciproque:
+            route = importeur.route.creer_route(derniere)
+            for salle in tuple(reversed(self.salles))[1:]:
+                route.ajouter_sortie(salle)
+
+            route.ajouter_sortie(self.origine)
+            route.completer(False)
