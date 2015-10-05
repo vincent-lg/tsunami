d73cb2e50baee10b1b5a30f04e59bc96681cddc7
Samuel Verschelde <stormi@laposte.net>
2015-10-05 09:37:23 +0200

    Vérifier que l'objet est un plateau avant de "jouer".
    Rapport 1639.

--- src/secondaires/jeux/commandes/jouer/__init__.py
+++ src/secondaires/jeux/commandes/jouer/__init__.py
@@ -63,6 +63,9 @@ class CmdJouer(Commande):
         personnage.agir("jouer")
         jeux = type(self).importeur.jeux.jeux
         plateaux = type(self).importeur.jeux.plateaux
+        if not objet.est_de_type("plateau de jeu"):
+            personnage << "|err|L'objet {} n'est pas un plateau de jeu.|ff|".format(objet)
+            return
         plateau = plateaux[objet.plateau]
         jeu = plateau.jeux[0]
         jeu = jeux[jeu]
