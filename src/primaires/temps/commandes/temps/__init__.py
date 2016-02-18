44fd9b7183f144e524fdb57503e1e5774a2261cc
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-02-17 22:27:39 -0800

    Correction de l'aide du temps IG

--- src/primaires/temps/commandes/temps/__init__.py
+++ src/primaires/temps/commandes/temps/__init__.py
@@ -39,6 +39,36 @@ from time import time
 from primaires.interpreteur.commande.commande import Commande
 
 # Constantes
+AIDE = """
+Permet de connaître la date et l'heure IG (in-game), c'est-à-dire
+le temps virtuel du MUD. Vous pouvez également utiliser cette commande
+pour convertir un temps réel en temps IG ou inversement, ce qui
+est parfois utile pour le RP. Par exemple, savoir quel est le temps
+in-game correspondant au premier janvier 2017.
+Pour ce faire, il suffit d'entrer le temps à convertir en paramètre
+de la commande %temps%. La première lettre informe le système si il
+s'agit d'un temps réel ou virtuel : |ent|r|ff| signifie que la date
+et l'heure qui suivent sont exprimées en temps réel, |ent|m|ff| indique
+que la date et 'heure précisé sont exprimées en temps virtuel
+(M pour MUD). Le format de la date et de l'heure à préciser est le suivant :
+    <année>-<mois>-<jour> <heure>:<minute>
+Vous pouvez omettre l'année et n'écrire que le mois, jour, heure et
+minute. Vous pouvez omettre le mois, ou même omettre le jour et ne préciser que l'heure. Voici quelques exemples possibles :
+
++-------------------+------------------------------------------------+
+| Paramètre         | Signification                                  |
++-------------------+------------------------------------------------+
+| r2017-01-01 00:00 | Le premier janvier 2017 à minuit               |
+| r05-18 15:30      | Le 18 mai de cette année à 15:30               |
+| r22 20:00         | Le 22 de ce mois et de cette année à 20:00     |
+| r 18:00           | Aujourd'hui à 18:00                            |
+| m50-01-01 00:00   | Le jour 1 du mois 1 de l'année 50 à minuit     |
+| m10-22 6:00       | Le jour 22 du mois 10 de cette année à 6:00    |
+| m15 19:45         | Le jour 15 de ce mois et cette année à 19:45   |
+| m 23:00           | Le même jour IG à 23:00                        |
++-------------------+------------------------------------------------+
+""".strip("\n")
+
 EXP = (
     re.compile("^(m)([0-9]+)\-([0-9]+)\-([0-9]+)\s+([0-9]+)\:([0-9]+)$", re.I),
     re.compile("^(m)([0-9]+)\-([0-9]+)\s+([0-9]+)\:([0-9]+)$", re.I),
@@ -61,8 +91,7 @@ class CmdTemps(Commande):
         Commande.__init__(self, "temps", "time")
         self.schema = "(<texte_libre>)"
         self.aide_courte = "affiche la date et l'heure de l'univers"
-        self.aide_longue = \
-            "Cette commande affiche la date et l'heure actuelles de l'univers."
+        self.aide_longue = AIDE
 
     def interpreter(self, personnage, dic_masques):
         """Méthode d'interprétation de commande"""
