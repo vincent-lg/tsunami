09423f63a31c1108229bf949599fa10837b3181d
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-06-20 16:18:08 -0700

    Ajout d'un code pour éviter la fuite de mémoire du module jeux

--- src/secondaires/jeux/plateaux/__init__.py
+++ src/secondaires/jeux/plateaux/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010-2016 DAVY Guillaume
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -42,25 +42,27 @@ Elle est détaillée plus bas.
 from abstraits.obase import BaseObj
 
 class BasePlateau(BaseObj):
-    
+
     """Classe définissant un plateau de jeu.
-    
+
     Un plateau est différent du
     jeu lui-même (on peut jouer à plusieurs jeux depuis le même plateau).
-    
+
     """
-    
+
     jeux = [] # liste des noms de jeux liés à ce plateau
     nom = "" # nom de ce plateau
-    def __init__(self):
+    def __init__(self, premiere=True):
         """Initialisation du plateau."""
         BaseObj.__init__(self)
-        self.init()
+        if premiere:
+            self.init()
+
         self._construire()
-    
+
     def __getnewargs__(self):
-        return ()
-    
+        return (False, )
+
     def afficher(self, personnage, jeu, partie):
         """Affiche la partie en cours au personnage."""
         return ""
