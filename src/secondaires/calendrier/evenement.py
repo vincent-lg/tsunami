baa36d71fec18d7be0c8793a73111d3ffa4da663
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-06-19 17:20:58 -0700

    Stabilisation du module secondaire calendrier

--- src/secondaires/calendrier/evenement.py
+++ src/secondaires/calendrier/evenement.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010-2016 AYDIN Ali-Kémal
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -38,19 +38,19 @@ from primaires.format.date import get_date
 from .commentaire import Commentaire
 
 class Evenement(BaseObj):
-    
+
     """Classe définissant un évènement pour le calendrier.
-    
+
     Un évènement est défini par un ID, qui est unique, un titre,
     une description courte, ainsi qu'une description longue, son
     créateur et ses responsables et enfin les commentaires lui
     correspondant.
-    
+
     """
-    
+
     enregistrer = True
     id_actuel = 1
-    
+
     def __init__(self, createur):
         """Constructeur d'un évènement"""
         BaseObj.__init__(self)
@@ -61,16 +61,17 @@ class Evenement(BaseObj):
         self.titre = "Sans Titre"
         self.description = Description(parent = self)
         self.commentaires = []
-    
+        self._construire()
+
     def __getnewargs__(self):
         return (None, )
-    
+
     def __repr__(self):
         return "<évènement {}>".format(self.id)
-    
+
     def __str__(self):
         return str(self.id)
-    
+
     @property
     def str_detail(self):
         """Renvoie une présentation détaillée de l'évènement."""
@@ -80,27 +81,35 @@ class Evenement(BaseObj):
                 "Date : Pour {date}\n" \
                 "Description :\n    {desc}\n" \
                 "Responsables : {resp}\n" \
-        
+
         nom_responsables = ", ".join([resp.nom for resp in self.responsables])
-        
+
         info_generale = info_generale.format(id=self.id,
             titre=self.titre, date=self.str_date, desc=self.description,
-            resp=nom_responsables)         
-        
+            resp=nom_responsables)
+
         commentaires = ""
         if self.commentaires:
             commentaires = "Commentaires :\n"
             for comm in self.commentaires:
                 commentaires += "\n{}".format(comm)
             info_generale += "\n\n" + commentaires
-        
+
         return info_generale
-    
+
     @property
     def str_date(self):
         """Retourne la date en format français."""
         return get_date(self.date.timetuple())
-    
+
     def ajouter_commentaire(self, personnage, commentaire):
         """Ajoute un commentaire"""
         self.commentaires.append(Commentaire(self, personnage, commentaire))
+        self._enregistrer()
+
+    def detruire(self):
+        """Destruction de l'évènement."""
+        BaseObj.detruire(self)
+        self.description.detruire()
+        for commentaire in self.commentaires:
+            commentaire.detruire()
