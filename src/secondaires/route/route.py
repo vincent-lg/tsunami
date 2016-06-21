a1adc6e9144fc45e18b877b699c2de34a584b6be
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-06-21 11:29:12 -0700

    Stabilisation du module secondaire route

--- src/secondaires/route/route.py
+++ src/secondaires/route/route.py
@@ -174,6 +174,7 @@ class Route(BaseObj):
 
         self.sorties.append(direction)
         self.salles.append(salle)
+        self._enregistrer()
         return direction
 
     def completer(self, reciproque=True):
@@ -196,6 +197,7 @@ class Route(BaseObj):
         derniere = self.salles[-1]
         self.destination = derniere
         importeur.route.ajouter_route(self)
+        self._enregistrer()
 
         if reciproque:
             route = importeur.route.creer_route(derniere)
