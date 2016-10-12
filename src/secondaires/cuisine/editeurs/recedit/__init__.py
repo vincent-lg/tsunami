3a46a62d573e8ca489e1bf94b52a668040070ce8
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2016-10-12 11:21:55 -0700

    Petite correction de formattage dans l'éditeur de l'XP de la recette

--- src/secondaires/cuisine/editeurs/recedit/__init__.py
+++ src/secondaires/cuisine/editeurs/recedit/__init__.py
@@ -2,10 +2,10 @@
 
 # Copyright (c) 2010-2016 NOEL-BARON Léo
 # All rights reserved.
-# 
+#
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # * Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # * Redistributions in binary form must reproduce the above copyright notice,
@@ -14,7 +14,7 @@
 # * Neither the name of the copyright holder nor the names of its contributors
 #   may be used to endorse or promote products derived from this software
 #   without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -39,6 +39,8 @@ les extensions n'apparaîtront pas ici.
 
 """
 
+from textwrap import dedent
+
 from primaires.interpreteur.editeur.presentation import Presentation
 from primaires.interpreteur.editeur.selection import Selection
 from primaires.interpreteur.editeur.entier import Entier
@@ -48,27 +50,27 @@ from .edt_resultat import EdtResultat
 from secondaires.cuisine.types.ustensile import Ustensile
 
 class EdtRecedit(Presentation):
-    
+
     """Classe définissant l'éditeur de recette 'recedit'.
-    
+
     """
-    
+
     nom = "recedit"
-    
+
     def __init__(self, personnage, recette):
         """Constructeur de l'éditeur"""
         if personnage:
             instance_connexion = personnage.instance_connexion
         else:
             instance_connexion = None
-        
+
         Presentation.__init__(self, instance_connexion, recette)
         if personnage and recette:
             self.construire(recette)
-    
+
     def __getnewargs__(self):
         return (None, None)
-    
+
     def construire(self, recette):
         """Construction de l'éditeur"""
         # Difficulté
@@ -81,7 +83,7 @@ class EdtRecedit(Presentation):
             "Entrez la |ent|difficulté|ff| de la recette entre |cmd|1|ff| " \
             "et |cmd|100|ff| ou |cmd|/|ff| pour revenir à\nla fenêtre " \
             "parente.\n\nDifficulté actuelle : |bc|{objet.difficulte}|ff|"
-        
+
         # Temps de cuisson
         temps = self.ajouter_choix("temps de cuisson", "t",
                 Entier, recette, "temps_cuisson", 1)
@@ -92,7 +94,7 @@ class EdtRecedit(Presentation):
             "Entrez le |ent|temps|ff| de cuisson en secondes " \
             "ou |cmd|/|ff| pour revenir à la fenêtre " \
             "parente.\n\nTemps actuel : |bc|{objet.temps_cuisson}|ff|"
-        
+
         # Cuisson
         cuisson = self.ajouter_choix("cuisson", "c", Choix, recette,
                 "cuisson", ["doux", "moyen", "vif"])
@@ -103,7 +105,7 @@ class EdtRecedit(Presentation):
             "Entrez la |ent|cuisson|ff| (feu |cmd|doux|ff|, |cmd|moyen|ff| " \
             "ou |cmd|vif|ff|) ou |cmd|/|ff| pour revenir à\nla fenêtre " \
             "parente.\n\nCuisson actuelle : |bc|feu {objet.cuisson}|ff|"
-        
+
         # Ustensiles
         l_ustensiles = list(Ustensile.types.keys())
         ustensiles = self.ajouter_choix("ustensiles", "u", Selection, recette,
@@ -117,7 +119,7 @@ class EdtRecedit(Presentation):
             "parente.\n\nTypes d'ustensiles existants : {}\n\nTypes " \
             "actuellement admis : |bc|{{objet.aff_ustensiles}}|ff|".format(
             ", ".join(l_ustensiles))
-        
+
         # Liste des ingrédients
         ingredients = self.ajouter_choix("ingrédients", "i",
                 EdtIngredients, recette, "ingredients")
@@ -125,7 +127,7 @@ class EdtRecedit(Presentation):
         ingredients.apercu = "{objet.aff_ingredients}"
         ingredients.aide_courte = \
             ""
-        
+
         # Résultat
         resultat = self.ajouter_choix("résultat", "r",
                 EdtResultat, recette, "resultat")
@@ -142,9 +144,16 @@ class EdtRecedit(Presentation):
                 Entier, recette, "xp", 0)
         xp.parent = self
         xp.prompt = "Expérience gagnée par le personnage en réussissant la recette : "
-        xp.apercu = "{objet.xp}"
-        xp.aide_courte = \
-            "Entrez l'|ent|expérience|ff| gagnée par le personnage qui réussit la recette. Le calcul de base conseillé est la difficulté de la recette multipliée par 500, le tout multiplié par le nombre d'ingrédients nécessaires et/ou le nombre de types d'ingrédients nécessaires, ou |cmd|/|ff| pour revenir à\nla fenêtre " \
-            "parente.\n\nExpérience gagnée actuelle : |bc|{objet.xp}|ff|"
-        
-        
+        xp.apercu = "{objet.xp} XP"
+        xp.aide_courte = dedent("""
+            Entrez l'|ent|expérience|ff| gagnée par le personnage.
+
+            Entrez |cmd|/|ff| pour revenir à la fenêtre parente.
+            Cette expérience est celle reçue dans le niveau survie par un
+            personnage réusissant la recette. Le calcul de base conseillé
+            est la difficulté de la recette multipliée par 50, le tout
+            multiplié par le nombre d'ingrédients nécessaires et/ou
+            le nombre de types d'ingrédients nécessaires.
+
+            Expérience gagnée actuelle : |bc|{objet.xp}|ff|
+        """.strip("\n"))
